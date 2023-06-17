90ce1a394f04040f7017cf1428ac5c56b98e875f
Youssefguba <Youssef.guba@abwaab.me>
2023-06-17 14:10:19 +0300

    Update Pref Keys & Lottie.

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -6,6 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Home'),
       ),
@@ -14,10 +15,66 @@ class HomeScreen extends StatelessWidget {
           // Slider
 
           // Category
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for (var i = 0; i < 50; i++) CategoryItemWidget(i: i),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(50, (i) => CategoryItemWidget(i: i)),
+            ),
+          ),
 
+          Container(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return CategoryItemWidget(i: index);
+              },
+            ),
+          ),
           // Flash Sale
         ],
       ),
+    );
+  }
+}
+
+class CategoryItemWidget extends StatelessWidget {
+  final int i;
+  const CategoryItemWidget({
+    super.key,
+    required this.i,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+          child: CircleAvatar(
+            radius: 32,
+            backgroundColor: Color(0xFFEBF0FF),
+            child: CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.person,
+                color: Color(0xff40BFFF),
+              ),
+            ),
+          ),
+        ),
+        Text('Shirts + ${i + 1}'),
+      ],
     );
   }
 }
--- lib/utils/prefs_key_constants.dart
+++ lib/utils/prefs_key_constants.dart
@@ -1,3 +1,4 @@
 class PrefKeys {
   static const String accessToken = 'access_token';
+
 }
